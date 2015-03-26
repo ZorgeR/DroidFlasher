3989ff5d8d6ed9f84e0b2da375e1f9f56ca178f2
zorg <zorg.rhrd@gmail.com>
2015-03-27 00:16:30 +0300

    r109.u1: - rebasing - refactoring

--- src/com/zlab/DroidFlasher/Controller.java
+++ src/com/zlab/DroidFlasher/Controller.java
@@ -420,7 +420,7 @@ public class Controller implements Initializable {
                             if(showResult){
 
                             args = args.replace("%RADIOBOXRESULT%", radioboxResult);
-                            args = args.replace("%SHOWRESULT%",Boolean.toString(showResult));
+                            args = args.replace("%SHOWRESULT%",Boolean.toString(true));
 
                             List<String> list = parseStringToArray(args);
                             String[] commands = list.toArray(new String[list.size()]);
@@ -1413,11 +1413,7 @@ public class Controller implements Initializable {
         alert.setContentText(text);
 
         Optional<ButtonType> result = alert.showAndWait();
-        if (result.get() == ButtonType.OK){
-            return true;
-        } else {
-            return false;
-        }
+        return result.get() == ButtonType.OK;
     }
     private boolean showDialogConfirmation(String title, String header, String text){
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
